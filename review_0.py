bd11b17dd1830e66b9ce1ad3019f010b3d5d56a7
gnxrlyqf <gnarlydestructor@outlook.com>
2023-11-14 00:52:55 +0100

    test

---
+++ review_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Doc
+"""
+from models.tmp_review import *
+from models.tmp_review import Review
+
+
+class Review(Review):
+    """Doc
+    """
+    place_id = None
+    user_id = None
+    text = None
