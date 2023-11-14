4477c0cab05168f358b92ba3f2a23627d00e4621
gnxrlyqf <gnarlydestructor@outlook.com>
2023-11-14 01:11:01 +0100

    test

--- models/place.py
+++ models/place.py
@@ -4,21 +4,7 @@ from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Place class
-
-    Attributes:
-        city_id (str): id of city
-        user_id (str): id of user
-        name (str): city name
-        description (str): city description
-        number_rooms (int): room count
-        number_bathrooms (int): bathroom count
-        max_guest (int): max guest count
-        price_by_night (int): price by night
-        latitude (float): place latitude
-        longitude (float): place longitude
-        amenity (list): amenity list
-    """
+    """Place class"""
     city_id = ""
     user_id = ""
     name = ""
